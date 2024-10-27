b46c5a941db26d2d9efd894547061290429d860f
torgav <christian.torgav@gmail.com>
2024-10-27 15:48:22 +0100

    side menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function myFunction(x) {
+  const offscreenwrapper = document.querySelector(".offscreen-wrapper");
   x.classList.toggle("change");
+  offscreenwrapper.classList.toggle("active");
 }
