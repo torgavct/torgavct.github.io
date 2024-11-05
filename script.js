cc6c6e164a7597e4cfb4266f7f31aa925e5e0354
torgav <christian.torgav@gmail.com>
2024-11-05 02:13:03 +0100

    upcoming page

--- script.js
+++
@@ -1,9 +0,0 @@
-function myFunction(x) {
-  const offscreenwrapper = document.querySelector(".offscreen-wrapper");
-  const herowrapper = document.querySelector(".hero-wrapper");
-  const navbar = document.querySelector(".navbar");
-  offscreenwrapper.classList.toggle("active");
-  herowrapper.classList.toggle("active");
-  navbar.classList.toggle("active");
-  x.classList.toggle("change");
-}
